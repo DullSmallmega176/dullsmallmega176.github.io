c971e7bdf9ea33b2e8508473afbbcdd253febde5
t4styl <helstrip2018@outlook.com>
2022-12-28 13:01:05 +0000

    added time

--- tiny-stocks/index.js
+++ tiny-stocks/index.js
@@ -1,11 +1,17 @@
 const hiddenElements = ['#light-mode' ,'#stat-tab'];
 const storage = window.localStorage;
 const default_game = {
+    money: 1000,
+    time: 360,
     settings: {
-        theme: 'dark'
+        theme: 'light'
     }
 };
 let game = default_game;
+
+function minutes_to_time(m) {
+    return `${Math.floor(m/60) % 24}:${(m % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}`
+} 
 
 $(document).ready(() => {
     for (const elt of hiddenElements) {
@@ -15,4 +21,11 @@ $(document).ready(() => {
     game = JSON.parse(storage.getItem('tiny-stocks') ?? JSON.stringify(default_game));
 
     $.getScript('themeSwitcher.js', () => switchTheme(game.settings.theme));
+
+    $('#time').html(minutes_to_time(game.time));
 });
+
+function update_time() {
+    game.time++;
+    $('#time').html(minutes_to_time(game.time));
+}
