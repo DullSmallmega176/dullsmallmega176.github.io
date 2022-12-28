cc26a14620f4d15173307ab6f62a1c5f5e78e172
t4styl <helstrip2018@outlook.com>
2022-12-28 09:10:19 +0000

    attempted to fix scrren flashing

--- tiny-stocks/themeSwitcher.js
+++ tiny-stocks/themeSwitcher.js
@@ -1,16 +1,16 @@
 function switchTheme(theme) {
     switch (theme) {
         case 'dark':
-            $('#dark-mode').hide();
-            $('#light-mode').show();
             $('html').addClass('dark');
             game.settings.theme = 'dark';
+            $('#dark-mode').hide();
+            $('#light-mode').show();
             storage.setItem('tiny-stocks', JSON.stringify(game));
             break;
         case 'light':
+            $('html').removeClass('dark');
             $('#dark-mode').show();
             $('#light-mode').hide();
-            $('html').removeClass('dark');
             game.settings.theme = 'light';
             storage.setItem('tiny-stocks', JSON.stringify(game));
             break;
