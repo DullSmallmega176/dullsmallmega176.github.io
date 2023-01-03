2c88679790d1629136395310c2ed2581026a29bc
t4styl <helstrip2018@outlook.com>
2023-01-03 13:30:37 +0000

    made stock info panel functional

--- tiny-stocks/index.js
+++ tiny-stocks/index.js
@@ -3,6 +3,27 @@ const storage = window.localStorage;
 const default_game = {
     money: 1000,
     time: 360,
+    stocks_owned: {
+        BTR: 0,
+        FLR: 0,
+        PZA: 0,
+        ICM: 0,
+        CHO: 0
+    },
+    stock_prices: {
+        BTR: 50,
+        FLR: 125,
+        PZA: 250,
+        ICM: 500,
+        CHO: 850
+    },
+    stock_prices_previous: {
+        BTR: 50,
+        FLR: 125,
+        PZA: 250,
+        ICM: 500,
+        CHO: 850
+    },
     settings: {
         theme: 'light'
     }
@@ -18,7 +39,7 @@ const stock_defaults = {
 let game = default_game;
 
 function minutes_to_time(m) {
-    return `${Math.floor(m/60) % 24}:${(m % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}`
+    return `${m > 719 && m < 780 ? '12' : Math.floor(m/60) % 12}:${(m % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})} ${Math.floor(m/60) % 24 < 12 ? 'am' : 'pm'}`;
 } 
 
 $(document).ready(() => {
@@ -29,6 +50,7 @@ $(document).ready(() => {
     game = JSON.parse(storage.getItem('tiny-stocks') ?? JSON.stringify(default_game));
 
     $.getScript('themeSwitcher.js', () => switchTheme(game.settings.theme));
+    $.getScript('stockInfo.js', () => switchInfo('btr'));
 
     $('#time').html(minutes_to_time(game.time));
 });
---
+++ tiny-stocks/stockInfo.js
@@ -0,0 +1,7 @@
+function switchInfo(s) {
+    let sCapital = s.toUpperCase();
+    $('#stock-info-name').html(sCapital);
+    $('#stock-info-amt').html(game.stocks_owned[sCapital]);
+    $('#stock-info-price').html(game.stock_prices[sCapital]);
+    $('#stock-info-inc').html(`${(game.stock_prices[sCapital] - game.stock_prices_previous[sCapital]) / game.stock_prices[sCapital] * 100}%`);
+}
